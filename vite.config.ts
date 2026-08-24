32e23874d004bf9f72d734312adaa3147b506e83
Prashant <317259075+CodingYetNahi@users.noreply.github.com>
2026-08-24 21:39:01 +0530

    Fix GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/Universal-Toolkit/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
